5e29d523191eaeb71fe2aa4939c1ed581f0311de
Joram van den Boezem <joram@vandenboezem.nl>
2019-04-06 23:29:08 +0200

    chore: try as shell script

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-docker login -u "$env:DOCKER_HUB_USER" -p "$env:DOCKER_HUB_PASSWORD"
+docker login -u "$DOCKER_HUB_USER" -p "$DOCKER_HUB_PASSWORD"
 docker build -f scripts/Dockerfile.windows -t exivity/base:latest .
 docker push exivity/base
-
