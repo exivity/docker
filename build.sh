ecc56c67d6b9c2a957592922b0eb2f951eb9ab72
Joram van den Boezem <joram@vandenboezem.nl>
2019-04-06 23:44:12 +0200

    fix: use different dockerfiles in build script

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
+#!/usr/bin/env bash
+
+if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
+    DOCKERFILE=Dockerfile
+elif [[ $TRAVIS_OS_NAME == 'windows' ]]; then
+    DOCKERFILE=Dockerfile.windows
+fi
+
 docker login -u "$DOCKER_HUB_USER" -p "$DOCKER_HUB_PASSWORD"
-docker build -f Dockerfile.windows -t exivity/base:latest .
+docker build -f "$DOCKERFILE" -t exivity/base:latest .
 docker push exivity/base
