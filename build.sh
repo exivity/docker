d27fcc87ac70c80950e5ecd6cd064c4130e0d8dd
Joram van den Boezem <joram@vandenboezem.nl>
2019-04-06 23:34:44 +0200

    chore: fix path

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 docker login -u "$DOCKER_HUB_USER" -p "$DOCKER_HUB_PASSWORD"
-docker build -f scripts/Dockerfile.windows -t exivity/base:latest .
+docker build -f Dockerfile.windows -t exivity/base:latest .
 docker push exivity/base
