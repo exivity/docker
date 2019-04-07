127ed0277fbe9ef826e5d5c4315cf4880c15ae26
Joram van den Boezem <joram@vandenboezem.nl>
2019-04-07 20:58:12 +0200

    chore: push manifest

--- manifest.sh
+++ manifest.sh
@@ -1,6 +1,12 @@
-docker -D manifest create "exivity/base:latest" `
-  "exivity/base:linux" `
+#!/usr/bin/env bash
+
+# @todo: verify experimental is enabled
+# echo '{ "experimental": "enabled" }' > ~/.docker/config.json
+
+docker -D manifest create "exivity/base:latest" \
+  "exivity/base:linux" \
   "exivity/base:windows"
-docker manifest annotate "exivity/base:latest" "exivity/base:linux" --os linux --arch amd64
-docker manifest annotate "exivity/base:latest" "exivity/base:windows" --os windows --arch amd64
+# @todo: is this needed?
+# docker manifest annotate "exivity/base:latest" "exivity/base:linux" --os linux --arch amd64
+# docker manifest annotate "exivity/base:latest" "exivity/base:windows" --os windows --arch amd64
 docker manifest push "exivity/base:latest"
