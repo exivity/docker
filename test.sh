c2534b8b01e2cf37e286ee4bf84f24ab44d874b1
Joram van den Boezem <joram@vandenboezem.nl>
2020-08-13 16:08:56 +0200

    chore: add tests

---
+++ test.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+if [[ $CI_PLATFORM == 'ubuntu-latest' ]]; then
+    PLATFORM=linux
+elif [[ $CI_PLATFORM == 'windows-latest' ]]; then
+    PLATFORM=windows
+fi
+
+LATEST_TAG=latest-$PLATFORM
+
+# https://gist.github.com/sj26/88e1c6584397bb7c13bd11108a579746
+function retry {
+  local retries=$1
+  shift
+
+  local count=0
+  until "$@"; do
+    exit=$?
+    wait=$((2 ** $count))
+    count=$(($count + 1))
+    if [ $count -lt $retries ]; then
+      echo "Retry $count/$retries exited $exit, retrying in $wait seconds..."
+      sleep $wait
+    else
+      echo "Retry $count/$retries exited $exit, no more retries left."
+      return $exit
+    fi
+  done
+  return 0
+}
+
+function get_health_status {
+  docker inspect --format="{{json .State.Health.Status}}" $IMAGE
+}
+
+function check_if_healthy {
+  status=`get_health_status`
+
+  echo "Got status $status"
+
+  [[ $status == "\"healthy\"" ]]
+}
+
+set -e
+
+echo "Running Docker image $IMAGE:$TAG in a container named $IMAGE"
+docker run \
+    --rm \
+    --detach \
+    --name $IMAGE \
+    exivity/$IMAGE:$LATEST_TAG
+
+echo "Running health check"
+# retry 5 means we will wait max 1+2+4+8+16 seconds
+retry 5 check_if_healthy
+
+echo "Stop Docker container $IMAGE"
+docker stop $IMAGE
