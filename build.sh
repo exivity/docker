55f5a1c3ffa2e9681e0d552dbdc930ddf9e30c96
Bogdan Marian <satrapu@users.noreply.github.com>
2022-08-14 17:24:01 +0300

    Rename variable for better readability

--- build.sh
+++ build.sh
@@ -1,30 +1,33 @@
 #!/usr/bin/env bash
 
-cd $IMAGE
-
 OS_PREFIX_FOR_LINUX='ubuntu'
 OS_PREFIX_FOR_WINDOWS='windows'
 
 if [[ $CI_PLATFORM == "$OS_PREFIX_FOR_LINUX"* ]]; then
-    PLATFORM=linux
+    DOCKER_PLATFORM='linux'
+    echo 'About to build Docker image for Linux'
 elif [[ $CI_PLATFORM == "$OS_PREFIX_FOR_WINDOWS"* ]]; then
-    PLATFORM=windows
+    DOCKER_PLATFORM='windows'
+    echo 'About to build Docker image for Windows ...'
 fi
 
-DOCKERFILE=Dockerfile.$PLATFORM
-VERSION_TAG=$VERSION-$PLATFORM
-LATEST_TAG=latest-$PLATFORM
+DOCKERFILE=Dockerfile.$DOCKER_PLATFORM
+VERSION_TAG=$VERSION-$DOCKER_PLATFORM
+LATEST_TAG=latest-$DOCKER_PLATFORM
+
+# Enter the folder containing Dockerfiles
+cd ./postgresql
 
 docker build \
     --build-arg VERSION=$VERSION \
     --build-arg EXTRA_BUILD_ARG=$EXTRA_BUILD_ARG \
     --file $DOCKERFILE \
-    --tag satrapu/$IMAGE:$VERSION_TAG \
+    --tag $DOCKER_IMAGE:$VERSION_TAG \
     .
 
-docker tag satrapu/$IMAGE:$VERSION_TAG satrapu/$IMAGE:$LATEST_TAG
+docker tag $DOCKER_IMAGE:$VERSION_TAG $DOCKER_IMAGE:$LATEST_TAG
 
 echo $DOCKER_HUB_TOKEN | docker login -u $DOCKER_HUB_USER --password-stdin
-docker push satrapu/$IMAGE:$VERSION_TAG
-docker push satrapu/$IMAGE:$LATEST_TAG
+docker push $DOCKER_IMAGE:$VERSION_TAG
+docker push $DOCKER_IMAGE:$LATEST_TAG
 
