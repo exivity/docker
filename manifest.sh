dd2d98652cdccbb61f1b8a32baf94f94f555665d
Bogdan Marian <satrapu@users.noreply.github.com>
2022-08-14 18:16:38 +0300

    Fix variable references

--- manifest.sh
+++ manifest.sh
@@ -4,17 +4,17 @@ export DOCKER_CLI_EXPERIMENTAL=enabled
 
 # Version
 docker manifest create \
-  "satrapu/$IMAGE:$POSTGRESQL_VERSION" \
-  "satrapu/$IMAGE:$POSTGRESQL_VERSION-linux" \
-  "satrapu/$IMAGE:$POSTGRESQL_VERSION-windows"
+  "satrapu/$DOCKER_IMAGE:$POSTGRESQL_VERSION" \
+  "satrapu/$DOCKER_IMAGE:$POSTGRESQL_VERSION-linux" \
+  "satrapu/$DOCKER_IMAGE:$POSTGRESQL_VERSION-windows"
 
 # Latest
 docker manifest create `
-  "satrapu/$IMAGE:latest" \
-  "satrapu/$IMAGE:latest-linux" \
-  "satrapu/$IMAGE:latest-windows"
+  "satrapu/$DOCKER_IMAGE:latest" \
+  "satrapu/$DOCKER_IMAGE:latest-linux" \
+  "satrapu/$DOCKER_IMAGE:latest-windows"
 
 echo $DOCKER_HUB_TOKEN | docker login -u $DOCKER_HUB_USER --password-stdin
 
-docker manifest push "satrapu/$IMAGE:$POSTGRESQL_VERSION"
-docker manifest push "satrapu/$IMAGE:latest"
+docker manifest push "$DOCKER_IMAGE:$POSTGRESQL_VERSION"
+docker manifest push "$DOCKER_IMAGE:latest"
