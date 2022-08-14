8182945d30c60973840cebb19bd5d4f6270db07c
Bogdan Marian <satrapu@users.noreply.github.com>
2022-08-14 18:42:58 +0300

    Fix Docker image references

--- manifest.sh
+++ manifest.sh
@@ -2,19 +2,18 @@
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
-# Version
+# Create manifests for specific Docker image version
 docker manifest create \
-  "satrapu/$DOCKER_IMAGE:$POSTGRESQL_VERSION" \
-  "satrapu/$DOCKER_IMAGE:$POSTGRESQL_VERSION-linux" \
-  "satrapu/$DOCKER_IMAGE:$POSTGRESQL_VERSION-windows"
+  "$DOCKER_IMAGE:$POSTGRESQL_VERSION" \
+  "$DOCKER_IMAGE:$POSTGRESQL_VERSION-linux" \
+  "$DOCKER_IMAGE:$POSTGRESQL_VERSION-windows"
 
-# Latest
+# Create manifests for latest Docker image version
 docker manifest create `
-  "satrapu/$DOCKER_IMAGE:latest" \
-  "satrapu/$DOCKER_IMAGE:latest-linux" \
-  "satrapu/$DOCKER_IMAGE:latest-windows"
+  "$DOCKER_IMAGE:latest" \
+  "$DOCKER_IMAGE:latest-linux" \
+  "$DOCKER_IMAGE:latest-windows"
 
 echo $DOCKER_HUB_TOKEN | docker login -u $DOCKER_HUB_USER --password-stdin
-
 docker manifest push "$DOCKER_IMAGE:$POSTGRESQL_VERSION"
 docker manifest push "$DOCKER_IMAGE:latest"
