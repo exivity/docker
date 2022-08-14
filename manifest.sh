ef62572e815bc618f44dc70cd51206588041daa7
Bogdan Marian <satrapu@users.noreply.github.com>
2022-08-14 17:09:00 +0300

    Increase Docker command readability

--- manifest.sh
+++ manifest.sh
@@ -3,12 +3,14 @@
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
 # Version
-docker manifest create "satrapu/$IMAGE:$VERSION" \
+docker manifest create \
+  "satrapu/$IMAGE:$VERSION" \
   "satrapu/$IMAGE:$VERSION-linux" \
   "satrapu/$IMAGE:$VERSION-windows"
 
 # Latest
-docker manifest create "satrapu/$IMAGE:latest" \
+docker manifest create `
+  "satrapu/$IMAGE:latest" \
   "satrapu/$IMAGE:latest-linux" \
   "satrapu/$IMAGE:latest-windows"
 
